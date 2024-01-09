1457be56db78ad6771fd4f6ee5ca254486d2c712
Emmah-oo <emmaochonogor715@gmail.com>
2024-01-09 10:04:17 +0100

    added clerk authentication

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+
+const Page = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default Page;
